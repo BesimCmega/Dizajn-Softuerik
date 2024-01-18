d5fe5d8dbab31631d9c7ad90e42573866b928fa6
BesimCmega <besimcmega7@gmail.com>
2024-01-18 13:24:53 +0100

    Fixing the Welcome at navbar but needs more change

--- resources/views/components/layouts/employee-navbar.blade.php
+++ resources/views/components/layouts/employee-navbar.blade.php
@@ -14,8 +14,8 @@
         <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 
           <li style="list-style-type: none" >
-            <span class="block py-2 px-3 text-gray-900 rounded md:border-0 md:p-0">
-              Welcome {{auth()->user()->firstname}} !
+            <span class="block py-2 px-3 dark:text-white rounded md:border-0 md:p-0">
+              Welcome {{auth()->user()->firstname}}!
             </span>
           </li>
 
