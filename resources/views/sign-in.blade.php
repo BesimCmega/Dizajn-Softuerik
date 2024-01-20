34fc22f9de1150c379ddf135f9ad2f31846e2276
BesimCmega <besimcmega7@gmail.com>
2024-01-20 23:47:58 +0100

    Removing the navbar from sign-in page

--- resources/views/sign-in.blade.php
+++ resources/views/sign-in.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body class="bg-blue-900">
 
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
+{{-- <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
@@ -33,11 +33,11 @@
         </ul>
       </div>
     </div>
-  </nav>
+  </nav> --}}
   
     <br><br><br>
 
-  <div class="flex justify-center">
+  <div class="flex justify-center pt-20">
 
     <div>
     <img class="h-80" src="{{ asset('images/panda2.png') }}" alt="">
